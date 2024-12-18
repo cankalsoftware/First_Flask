6a6ce87177b46f7d02b522d67e4c7f85e6db011a
Ali Cankal <uyko76@yahoo.com>
2024-12-18 11:54:27 +0000

    Update app.py
    update render settings

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ def hello_template():
     return render_template("hello_there.html", name=names, date=datetime.now())
 
 if __name__ == "__main__":
+    # port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=5000, debug=True)
 
     
